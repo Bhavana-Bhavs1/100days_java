3df32516afba64e45b09edfd1d640dc6a2d173d2
Bhavana-Bhavs1 <68940386+Bhavana-Bhavs1@users.noreply.github.com>
2020-08-13 14:47:30 +0530

    Add files via upload

---
+++ File2.java
@@ -0,0 +1,19 @@
+package Pack1;
+import java.util.Scanner;
+
+public class File2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		Scanner obj=new Scanner(System.in);
+		System.out.println("enter num1");
+		int a=obj.nextInt();
+		System.out.println("enter num2");
+		int b=obj.nextInt();
+		int c=a+b;
+		System.out.println("sum="+c);
+		
+
+	}
+
+}
