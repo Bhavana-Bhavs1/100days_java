353922c1a6e5d1dc662a9b752dbbf960f19d3bc3
Bhavana-Bhavs1 <68940386+Bhavana-Bhavs1@users.noreply.github.com>
2020-08-13 14:48:17 +0530

    Add files via upload

---
+++ File3.java
@@ -0,0 +1,23 @@
+package Pack1;
+
+import java.util.Scanner;
+
+public class File3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enetr sno");
+		int sno=obj.nextInt();
+		System.out.println("Enter sname");
+		String sname=obj.next();
+		System.out.println("Enter age");
+		int age=obj.nextInt();
+		System.out.println("Enter fee");
+		float fee=obj.nextFloat();
+		System.out.println("sno="+sno+"\n sname="+sname+"\n age="+age+"\nfee="+fee);
+		
+
+	}
+
+}
