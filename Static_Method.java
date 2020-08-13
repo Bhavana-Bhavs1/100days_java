28330e49c463038f29b13220bc656386413aab85
Bhavana-Bhavs1 <68940386+Bhavana-Bhavs1@users.noreply.github.com>
2020-08-13 14:50:34 +0530

    Add files via upload

---
+++ Static_Method.java
@@ -0,0 +1,19 @@
+package Pack1;
+class Test1
+{
+	static int x =10;
+	static void show()
+	{
+		System.out.println(" static value = "+ x);
+	}
+}
+
+public class Static_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test1.show();
+
+	}
+
+}
