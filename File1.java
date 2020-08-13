8d59d7e5c4a08eff7c15427eb5cfdd2473481e91
Bhavana-Bhavs1 <68940386+Bhavana-Bhavs1@users.noreply.github.com>
2020-08-13 14:47:06 +0530

    Add files via upload

---
+++ File1.java
@@ -0,0 +1,15 @@
+package Pack1;
+
+public class File1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("sum="+c);
+		
+
+	}
+
+}
