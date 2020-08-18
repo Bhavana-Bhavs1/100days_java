d2366e1ac368ec21294df7178d74d7b26de966ac
Madala Bhavana Rani Patnaik <68940386+Bhavana-Bhavs1@users.noreply.github.com>
2020-08-18 13:40:00 +0530

    Add files via upload

---
+++ Farenhiet.java
@@ -0,0 +1,17 @@
+package Basic_Prgs;
+import java.util.*;
+
+public class Farenhiet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc = new Scanner(System.in);
+		  float f,c;
+		  System.out.println("enter Farenheit temperature:");
+		  f = sc.nextFloat();
+		  c = ((f-32)*5)/9;
+		  System.out.println("Celsius = "+c);
+
+	}
+
+}
