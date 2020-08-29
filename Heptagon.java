85626c4ab239ccd349cf03df6e749efa79f15bff
Madala Bhavana Rani Patnaik <68940386+Bhavana-Bhavs1@users.noreply.github.com>
2020-08-29 11:05:05 +0530

    Add files via upload

---
+++ Heptagon.java
@@ -0,0 +1,15 @@
+package Basic_Prgs;
+import java.util.*;
+public class Heptagon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner obj = new Scanner(System.in);
+	      System.out.println("enter length of a side ");
+	      float s = obj.nextFloat();
+	      float a = 3.634f * s * s;
+	      System.out.println("Area of Heptagon = "+ a);
+
+	}
+
+}
