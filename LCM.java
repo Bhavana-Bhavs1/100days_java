7484dfcbe2b4c4a16d4e887040be66b2cdf2c640
Madala Bhavana Rani Patnaik <68940386+Bhavana-Bhavs1@users.noreply.github.com>
2020-08-29 11:16:11 +0530

    Add files via upload

---
+++ LCM.java
@@ -0,0 +1,25 @@
+package Progs_2;
+import java.util.*;
+public class LCM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter 2 numbers:");
+		int n1 = sc.nextInt(); //12
+		int n2 = sc.nextInt(); //24
+		int gcd = 1;
+		for(int i = 1; i<=n1 && i<=n2;i++)
+		{
+			if(n1%i ==0 && n2%i ==0)
+			{
+				gcd = i;
+			}
+		}
+	     int lcm = (n1*n2)/gcd;  // (12*24)/12
+		System.out.println(lcm); //24
+
+	}
+
+}
